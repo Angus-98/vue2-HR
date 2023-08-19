86a88792a2fe4b9e2b632c277072f7a77980768a
Angus-98 <woshiniuncle@gmail.com>
2023-08-20 00:30:46 +0800

    scss的基本语法以及使用

---
+++ src/views/test/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="test">
+    <h2 class="test-h2">scss基本语法以及使用</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+  created() {},
+  methods: {}
+}
+</script>
+
+<style lang="scss" scoped>
+// 通过$符号声明全局变量
+$highLight-color:#87ceeb;
+.test {
+  .test-h2 {
+    font-size: 30px;
+    color: #666;
+    // &符号 表示的就是 h2标签
+    &:hover {
+      color: $highLight-color;
+    }
+  }
+}
+</style>
