76a291a2c8ee22ea5b54e60aa099ff9a43a60bfd
Angus-98 <woshiniuncle@gmail.com>
2023-08-20 00:50:23 +0800

    scss局部变量以及(@mixin-@include)

--- src/views/test/index.vue
+++ src/views/test/index.vue
@@ -19,13 +19,24 @@ export default {
 <style lang="scss" scoped>
 // 通过$符号声明全局变量
 $highLight-color:#87ceeb;
+// 通过@mixin 声明代码块，@inclide调用
+@mixin box {
+  width: 200px;
+  height: 200px;
+  border: 1px solid #000;
+  border-radius: 20px;
+}
+
 .test {
+  @include box; //使用代码块
   .test-h2 {
     font-size: 30px;
     color: #666;
     // &符号 表示的就是 h2标签
     &:hover {
+      $backgroundColor: #003666; //局部变量,其它代码块是使用不了的
       color: $highLight-color;
+      background-color: $backgroundColor;
     }
   }
 }
